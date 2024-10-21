2a5d3172fca0fe40e7b50f589f63e34226abc0e4
Wilovy09 <srwilovy09@gmail.com>
2024-10-21 02:22:49 -0600

    :sparkles: Add bottom navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -3,8 +3,32 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
+  <nav
+    class="text-gray-font border-gray-99 fixed bottom-0 z-40 flex h-16 w-full justify-between border-t bg-white px-6 py-2 shadow-lg"
+  >
+    <RouterLink to="/">
+      <span
+        class="text-primary flex cursor-pointer flex-col items-center px-2 py-1 text-center text-sm hover:bg-gray-200 hover:text-gray-700"
+      >
+        <i class="fas fa-home w-8 p-1"> </i>
+        <span class="font-roboto mx-1">Home</span>
+      </span>
+    </RouterLink>
+    <RouterLink to="/">
+      <span
+        class="flex cursor-pointer flex-col items-center px-2 py-1 text-center text-sm hover:bg-gray-200 hover:text-gray-700"
+      >
+        <i class="fas fa-envelope w-8 p-1"> </i>
+        <span class="font-roboto mx-1">Pesan</span>
+      </span>
+    </RouterLink>
+    <RouterLink to="/">
+      <span
+        class="flex cursor-pointer flex-col items-center px-2 py-1 text-center text-sm hover:bg-gray-200 hover:text-gray-700"
+      >
+        <i class="fas fa-user-circle w-8 p-1"> </i>
+        <span class="font-roboto mx-1">Akun</span>
+      </span>
+    </RouterLink>
   </nav>
 </template>
